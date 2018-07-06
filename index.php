aad9f9b8ace4249c274012ade54bd467926b2fec
pavel02 <pavel.hr@mail.ru>
2018-07-06 19:30:16 +0300

    Работа с HTTP теперь через компонент Symfony.

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-$input = isset($_GET['name']) ? $_GET['name'] : 'World';
+require_once __DIR__ . '/vendor/autoload.php';
 
-header('Content-Type: text/html; charset=utf-8');
+use Symfony\Component\HttpFoundation\{Request, Response};
 
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+// новый объект, основанный на текущих глобальных переменных PHP
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new  Response(printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
