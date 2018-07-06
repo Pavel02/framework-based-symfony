9d5f293cf9ddc190bb4fe13300a14c07d9314e0c
pavel02 <pavel.hr@mail.ru>
2018-07-06 20:57:35 +0300

    новая структура каталогов

--- framework/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\{Request, Response};
-
-$request = Request::createFromGlobals();
-$response = new Response();
