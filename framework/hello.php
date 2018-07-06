2e8b84e55f31425434f1cdffdbdf2eff9e5b8c17
pavel02 <pavel.hr@mail.ru>
2018-07-06 20:48:58 +0300

    добавлен фронт контроллер

--- framework/hello.php
+++ framework/hello.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once __DIR__ . '/init.php';
-
 $input = $request->get('name', 'World');
 
 $response->setContent(printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-$response->send();
