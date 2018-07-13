b7cc59e5143535c1ea4132799e46c1b901f0d837
pavel02 <pavel.hr@mail.ru>
2018-07-13 12:40:07 +0300

    подготовка ответа через контроллер

--- web/front.php
+++ web/front.php
@@ -5,14 +5,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing;
 
-function render_template($request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
+//function render_template($request)
+//{
+//    extract($request->attributes->all(), EXTR_SKIP);
+//    ob_start();
+//    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
+//
+//    return new Response(ob_get_clean());
+//}
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__ . '/../src/app.php';
@@ -23,7 +23,10 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func('render_template', $request);
+    var_dump('<pre>');
+    var_dump($request);
+//    $response = call_user_func('render_template', $request);
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found Page', 404);
 } catch (Exception $e) {
